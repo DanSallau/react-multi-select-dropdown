ae37d051331d43322782d7e81750d227021a23f9
Nuru <nuruddeensalihu@yahoo.com>
2018-08-26 12:22:05 +0800

    Initial commit, project structure and the compont skeleton

---
+++ src/components/DropDown.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+
+export default class DropDownMultiSelect extends React.PureComponent<any, {}> {
+    render() {
+        return(
+            <h1>Welcome to Dropdown</h1>
+        )
+    }
+}
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+
+import { render } from 'react-dom';
+import DropDownMultiSelect  from './components/DropDown';
+
+render(<DropDownMultiSelect />, document.getElementById('app') as HTMLElement)
